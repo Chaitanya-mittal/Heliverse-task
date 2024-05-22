2eece1f14cea6bc4e5673e8c00f50f767b65689f
chaitanya <cm@cm.com>
2024-05-22 10:27:52 +0530

    2.1

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,11 +2,14 @@ import "./Navbar.css";
 function Navbar() {
   return (
     <nav className="fixed z-10 flex h-[54px] w-full items-center justify-between bg-[#262626] ps-[10px]">
-      <img
-        src="logo.svg"
-        alt="logo"
-        className="w-[8rem] ps-[10px] md:w-[10rem]"
-      />
+      <a href="https://codecanyon.net/">
+        <img
+          src="logo.svg"
+          alt="logo"
+          className="w-[8rem] ps-[10px] md:w-[10rem]"
+        />
+      </a>
+
       <div className="flex h-full items-center justify-center pe-[10px] md:px-[20px]">
         <a
           href="https://codecanyon.net/checkout/102342108/create_account"
